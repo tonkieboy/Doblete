100e972a84767abe4553d5a51daf81e51d885dad
John Haeron <millena2214385@ceu.edu.ph>
2025-04-30 18:12:52 +0800

    Hi

--- prisma/seed.js
+++ prisma/seed.js
@@ -14,6 +14,8 @@ const userData = {
       { task: "Schedule Appointment" },
       { task: "Review for Exam" },
       { task: "Experimental Code" },
+      { task: "Wala Lang" },
+
     ],
   },
 };
